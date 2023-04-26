5afa4da15501ce4be5ad385e40fa0463d136f988
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2023-04-26 12:14:42 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -22,72 +22,74 @@ with st.sidebar:
 
 # Heart Disease Prediction Page
 if (selected == 'Heart Disease Prediction'):
-    
     # page title
     st.title('Heart Disease Prediction using ML')
     
     col1, col2, col3 = st.columns(3)
     
     with col1:
+
         age = st.slider('Age', 1, 100, 25)
+
+        trtbps = st.slider('Resting Blood Pressure', 80, 200, 120)
+
+        thalachh = st.slider('Maximum Heart Rate achieved', min_value=60, max_value=220, value=120)
+
+        oldpeak = st.number_input('ST depression induced by exercise', value=0.0, step=0.1)
+
+        thal_options = ['Normal', 'Fixed Defect', 'Reversible Defect']
+        thall = st.selectbox('Thal', options=thal_options)
+        thall = 0 if thall == 'Normal' else (1 if thall == 'Fixed Defect' else 2)
+
         
     with col2:
+
         sex_options = ['Male', 'Female']
         sex = st.selectbox('Sex', options=sex_options)
         sex = 0 if sex == 'Female' else 1
-        
+
+        chol = st.text_input('Serum Cholesterol in mg/dl')
+
+        restecg_options = [0, 1, 2]
+        restecg_labels = ['Normal', 'Abnormality in ST-T wave', 'Showing probable or definite left ventricular hypertrophy']
+        restecg = st.selectbox('Resting Electrocardiographic Results', options=restecg_options, format_func=lambda x: restecg_labels[x])
+
+        slope_options = [0, 1, 2]
+        slp = st.selectbox('Slope of the peak exercise ST segment', options=slope_options)
+    
     with col3:
-        cp = st.text_input('Chest Pain')
+
+        cp_options = ['Typical Angina', 'Atypical Angina', 'Non-Anginal Pain', 'Asymptomatic']
+        cp = st.selectbox('Chest Pain type', options=cp_options)
+        cp = 0 if cp == 'Typical Angina' else (1 if cp == 'Atypical Angina' else (2 if cp == 'Non-Anginal Pain' else 3))
+
+        fbs_options = ['False', 'True']
+        fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', options=fbs_options)
+        fbs = 0 if fbs == '< 120' else 1
+
+        exang_options = ['No', 'Yes']
+        exng = st.selectbox('Exercise induced angina', options=exang_options)
+        exng = 0 if exng == 'No' else 1
+    
+        caa_options = ['0', '1', '2', '3', '4']
+        caa = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', options=caa_options)
+
         
-    with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
-        
-    with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
-        
-    with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-        
-    with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
-        
-    with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
-        
-    with col3:
-        exang = st.text_input('Exercise Induced Angina')
-        
-    with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
-        
-    with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
-        
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-        
-    with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-        
-        
-     
-     
     # code for Prediction
+
     heart_diagnosis = ''
-    
     # creating a button for Prediction
-    
+
     if st.button('Heart Disease Test Result'):
-        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
+
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall]])                          
         
         if (heart_prediction[0] == 1):
-          heart_diagnosis = 'The person is having heart disease'
+            heart_diagnosis = 'The person is having heart disease'
         else:
-          heart_diagnosis = 'The person does not have any heart disease'
-        
+            heart_diagnosis = 'The person does not have any heart disease'
+
     st.success(heart_diagnosis)
-        
-    
     
 
 
