ba89777088ce4ee731daf7d2d61920bf622c1bcc
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2023-04-26 13:05:27 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -9,19 +9,47 @@ heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
-    
     selected = option_menu('Main Menu',
-                          
-                          [
-                           'Heart Disease Prediction'
-                           ],
+                          ['About the Project', 'Heart Disease Prediction',  'Attributes Description', 'Analysis and Visualization', 'Findings and Outcomes'],
                           icons=['heart'],
                           default_index=0)
-  
 
+if (selected == 'About the Project'):
+
+    st.title("Project Description")
+
+    st.markdown("""
+    Cardiovascular diseases (CVDs) are the leading cause of death globally, taking an estimated 17.5 million lives each year, accounting for nearly 31 percent of global deaths. It is important to identify those at the highest risks of CVDs to ensure appropriate treatment and preventing premature deaths.
+    
+    This project is a web application that predicts the presence of heart disease in a patient based on various medical factors such as age, blood pressure, cholesterol level, etc. The project uses machine learning algorithms to analyze the data and make predictions.
+
+    ### Data
+
+    The data used in this project is the [Heart Disease UCI dataset](https://archive.ics.uci.edu/ml/datasets/Heart+Disease) from the UCI Machine Learning Repository. The dataset contains 303 samples with 14 features, including age, sex, chest pain type, resting blood pressure, serum cholesterol, fasting blood sugar level, resting electrocardiographic results, maximum heart rate achieved, exercise-induced angina, ST depression induced by exercise relative to rest, slope of the peak exercise ST segment, number of major vessels colored by fluoroscopy, and thalassemia.
+    
+    ### Machine Learning Algorithm
+
+    The machine learning algorithm used for this project is [Support Vector Machine](https://scikit-learn.org/stable/modules/svm.html). The model is able to predict whether a patient has heart disease with an accuracy of 93.44%.
+
+    ### Libraries Used
+
+    This project uses the following libraries:
+
+    * Pandas
+    * NumPy
+    * Scikit-learn
+    * Matplotlib
+    * Seaborn
+    * Streamlit
+
+    ### About the Developer
+
+    This project was developed by Vatsal Parikh. You can find more of my projects on [GitHub](https://github.com/vatsalparikh07).
+
+    """)
 
 # Heart Disease Prediction Page
-if (selected == 'Heart Disease Prediction'):
+elif (selected == 'Heart Disease Prediction'):
     # page title
     st.title('Heart Disease Prediction using ML')
     
@@ -72,7 +100,7 @@ if (selected == 'Heart Disease Prediction'):
         exng = 0 if exng == 'No' else 1
     
         caa_options = ['0', '1', '2', '3', '4']
-        caa = st.selectbox('Major Vessels Colored by Fluoroscopy', options=caa_options)
+        caa = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', options=caa_options)
 
         
     # code for Prediction
@@ -85,12 +113,17 @@ if (selected == 'Heart Disease Prediction'):
         heart_prediction = heart_disease_model.predict([[age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall]])                          
         
         if (heart_prediction[0] == 1):
-            heart_diagnosis = 'The person is likely to have a heart disease'
+            heart_diagnosis = 'The person is having heart disease'
         else:
-            heart_diagnosis = 'The person is unlikely to have a heart disease'
+            heart_diagnosis = 'The person does not have any heart disease'
 
     st.success(heart_diagnosis)
-    
+
+elif (selected == 'Attributes Description'):
+    st.set_page_config(page_title="Attributes of Heart Disease Prediction", page_icon=":heart:")
+
+    # Define page header
+    st.markdown("# Attributes of Heart Disease Prediction")
 
 
 
